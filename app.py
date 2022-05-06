771351b1b2c09debd796befe91a262bbc8bf1baf
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-05-06 19:59:53 +0900

    timeattack

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+import hashlib
+import certifi  #만약 몽고 디비 돌릴때 문제가 없으셨다면 해당 줄은 주석 처리 하세요.
+client = MongoClient('mongodb+srv://test:sparta@cluster0.0mzan.mongodb.net/Cluster0?retryWrites=true&w=majority' ,tlsCAFile=certifi.where())
+db = client.dbsparta
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('home.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    email = request.form["email"]
+    password = request.form["password"]
+
+    pass_ch = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+    doc = {'email':email, 'password':pass_ch}
+
+    db.timeattack.insert_one(doc)
+    print(doc)
+    return jsonify()
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
